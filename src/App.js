a34e8d6367785f2abd606e44cb193f49a33a2576
Ben Monro <benjamin.monro@walmartlabs.com>
2020-03-13 10:28:54 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [showStuff, setShowStuff] = useState(false);
+  function handleClick() {
+    setTimeout(() => {
+
+      setShowStuff(true);
+    }, 500)
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={handleClick}>click me</button>
+        {showStuff && <div>Hello world</div>}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
@@ -7,3 +7,11 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("should match snapshot async", async () => {
+  render(<App />);
+  // fireEvent.click(screen.getByText("click me"));
+
+  // waitFor()
+  expect(document.body).toMatchSnapshot();
+})
